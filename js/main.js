9a7482047d42520aad60c1dbe09569cf47b63a96
Marcus Hansson <Marcus.Hansson@axson.local>
2017-08-11 16:25:21 +0200

    started on jQuery code

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// Set startpicture and a variabel for next
+var sliderInt = 1;
+var sliderNext = 2;
+
+$(document).ready(function(){
+  $(".slider img#1").fadeIn(300);
+});
+
+// 5:54 youtube vid: https://www.youtube.com/watch?v=z277ZUHNnnE
