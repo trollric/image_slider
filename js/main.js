68a9b1ffb472a1188feaded3f87610770afed3fb
Marcus Hansson <Marcus.Hansson@axson.local>
2017-08-17 13:55:39 +0200

    Fixed some functions not working. Added a loop

--- js/main.js
+++ js/main.js
@@ -3,7 +3,18 @@ var sliderInt = 1;
 var sliderNext = 2;
 
 $(document).ready(function(){
-  $(".slider img#1").fadeIn(300);
+  $("#slider>img#1").fadeIn(300);
+  startSlider();
 });
 
+function startSlider(){
+  var count = $("#slider>img").length;
+  var loop = setInterval(function(){
+    $("#slider>img").fadeOut(300);
+    $("#slider>img#"+sliderNext).fadeIn(300);
+    sliderInt = sliderNext;
+    sliderNext = sliderNext+1;
+  },3000);
+
+}
 // 5:54 youtube vid: https://www.youtube.com/watch?v=z277ZUHNnnE
